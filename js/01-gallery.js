b51b2ad2a33bcd27f68592c70490274c7ad3a071
sergiihsv <hsv-serg@ukr.net>
2021-12-15 15:53:18 +0200

    hw-07 01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,21 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+
+const createImageMarkup = galleryItems.map((item) => {
+  return;
+
+  <div class="gallery__item">
+    <a class="gallery__link" href="large-image.jpg">
+      <img
+        class="gallery__image"
+        src="small-image.jpg"
+        data-source="large-image.jpg"
+        alt="Image description"
+      />
+    </a>
+  </div>;
+});
